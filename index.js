4bc98f2ad6cbefd692de4c24b97e77766c4bd029
ruru <ruru.dev07@gmail.com>
2024-01-31 18:53:35 +0530

    FIX p1 (#2)

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Set up Multer for file uploads
 const storage = multer.diskStorage({
-  destination: 'uploads/',  // Specify the folder to store uploaded images
+  destination: "uploads/", // Specify the folder to store uploaded images
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     const fileExtension = path.extname(file.originalname);
-    cb(null, file.fieldname + '-' + uniqueSuffix + fileExtension);
-  }
+    cb(null, file.fieldname + "-" + uniqueSuffix + fileExtension);
+  },
 });
 
 const upload = multer({ storage: storage });
 
 // Serve uploaded images
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
 // POST endpoint for uploading images
-app.post('/upload', upload.single('profileImage'), (req, res) => {
+app.post("/upload", upload.single("profileImage"), (req, res) => {
   // Access the uploaded file through req.file
   if (!req.file) {
-    return res.status(400).json({ error: 'No file uploaded' });
+    return res.status(400).json({ error: "No file uploaded" });
   }
 
   // Store the file information in a database or use Moltin to handle storage
 
-  return res.json({ success: true, message: 'File uploaded successfully' });
+  return res.json({ success: true, message: "File uploaded successfully" });
 });
 
 // GET endpoint to retrieve an image by a link
-app.get('/get-image/:imageName', (req, res) => {
+app.get("/get-image/:imageName", (req, res) => {
   const imageName = req.params.imageName;
-  const imagePath = path.join(__dirname, 'uploads', imageName);
+  const imagePath = path.join(__dirname, "uploads", imageName);
 
   // Check if the file exists
   if (fs.existsSync(imagePath)) {
     return res.sendFile(imagePath);
   } else {
-    return res.status(404).json({ error: 'Image not found' });
+    return res.status(404).json({ error: "Image not found" });
   }
 });
 
@@ -50,3 +50,5 @@ app.get('/get-image/:imageName', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
